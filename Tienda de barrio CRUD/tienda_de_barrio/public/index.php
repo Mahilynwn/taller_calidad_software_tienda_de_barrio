8bf64af9c84308d4a29982730492651dd19e3866
Mahilynwn <mahilyg17@gmail.com>
2025-10-18 00:59:17 -0500

    docs sonnar solution

--- Tienda de barrio CRUD/tienda_de_barrio/public/index.php
+++ Tienda de barrio CRUD/tienda_de_barrio/public/index.php
@@ -16,7 +16,7 @@ define('LARAVEL_START', microtime(true));
 |
 */
 
-require __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
